f89f8a22b0c15c821610bdbba10a76edc3d17652
Mauro Mascarenhas de Araújo <mauromascarenhas@users.noreply.github.com>
2022-12-07 18:36:23 -0300

    fix(slider): slider accessibility
    Improve slider support to assistive technologies (e.g.: screen readers):
    
    - Add "pause" options to slider initialization:
        - pauseOnFocus: Slider should pause when receive keyboard focus;
        - pauseOnHover: Slider should pause when hovered by mouse;
    - New "indicatorLabelFunc": Must generate ARIA labels for indicators;
    - New eventPause prop: is true if slider is paused by focus/hover event;
    - Slider must receive id if does not have one;
    - Sliders are able to receive focus by code;
    - Non-current slides are also hidden in CSS (also from screen readers);
    - Add button to indicators:
        - Update CSS;
        - Update event handlers.
    - Indicator click should focus current slide.

--- js/slider.js
+++ js/slider.js
@@ -5,7 +5,10 @@
     indicators: true,
     height: 400,
     duration: 500,
-    interval: 6000
+    interval: 6000,
+    pauseOnFocus: true,
+    pauseOnHover: true,
+    indicatorLabelFunc: null // Function which will generate a label for the indicators (ARIA)
   };
 
   /**
@@ -31,8 +34,17 @@
        * @prop {Number} [height=400] - height of slider
        * @prop {Number} [duration=500] - Length in ms of slide transition
        * @prop {Number} [interval=6000] - Length in ms of slide interval
+       * @prop {Boolean} [pauseOnFocus=true] - Pauses transition when slider receives keyboard focus
+       * @prop {Boolean} [pauseOnHover=true] - Pauses transition while mouse hovers the slider
        */
       this.options = $.extend({}, Slider.defaults, options);
+
+      // init props
+      this.interval = null;
+      this.eventPause = false;
+      this._hovered = false;
+      this._focused = false;
+      this._focusCurrent = false;
 
       // setup
       this.$slider = this.$el.find('.slides');
@@ -49,6 +61,13 @@
 
       this._setSliderHeight();
 
+      // Sets element id if it does not have one
+      if (this.$slider[0].hasAttribute("id")) this._sliderId = this.$slider[0].getAttribute("id");
+      else {
+        this._sliderId = "slider-" + M.guid();
+        this.$slider[0].setAttribute("id", this._sliderId);
+      }
+
       // Set initial positions of captions
       this.$slides.find('.caption').each((el) => {
         this._animateCaptionIn(el, 0);
@@ -63,12 +82,19 @@
           $(el).attr('src', placeholderBase64);
         }
       });
+      this.$slides.each((el) => {
+        // Sets slide as focusable by code
+        if (!el.hasAttribute("tabindex")) el.setAttribute("tabindex", -1);
+        // Removes initial visibility from "inactive" slides
+        el.style.visibility = 'hidden';
+      });
 
       this._setupIndicators();
 
       // Show active slide
       if (this.$active) {
         this.$active.css('display', 'block');
+        this.$active[0].style.visibility = 'visible';
       } else {
         this.$slides.first().addClass('active');
         anim({
@@ -77,13 +103,14 @@
           duration: this.options.duration,
           easing: 'easeOutQuad'
         });
+        this.$slides.first()[0].style.visibility = 'visible';
 
         this.activeIndex = 0;
         this.$active = this.$slides.eq(this.activeIndex);
 
         // Update indicators
         if (this.options.indicators) {
-          this.$indicators.eq(this.activeIndex).addClass('active');
+          this.$indicators.eq(this.activeIndex).children().first().addClass('active');
         }
       }
 
@@ -137,10 +164,23 @@
     _setupEventHandlers() {
       this._handleIntervalBound = this._handleInterval.bind(this);
       this._handleIndicatorClickBound = this._handleIndicatorClick.bind(this);
+      this._handleAutoPauseFocusBound = this._handleAutoPauseFocus.bind(this);
+      this._handleAutoStartFocusBound = this._handleAutoStartFocus.bind(this);
+      this._handleAutoPauseHoverBound = this._handleAutoPauseHover.bind(this);
+      this._handleAutoStartHoverBound = this._handleAutoStartHover.bind(this);
+      
+      if (this.options.pauseOnFocus){
+        this.el.addEventListener('focusin', this._handleAutoPauseFocusBound);
+        this.el.addEventListener('focusout', this._handleAutoStartFocusBound);
+      }
+      if (this.options.pauseOnHover){
+        this.el.addEventListener('mouseenter', this._handleAutoPauseHoverBound);
+        this.el.addEventListener('mouseleave', this._handleAutoStartHoverBound);
+      }
 
       if (this.options.indicators) {
         this.$indicators.each((el) => {
-          el.addEventListener('click', this._handleIndicatorClickBound);
+          el.children[0].addEventListener('click', this._handleIndicatorClickBound);
         });
       }
     }
@@ -149,9 +189,17 @@
      * Remove Event Handlers
      */
     _removeEventHandlers() {
+      if (this.options.pauseOnFocus){
+        this.el.removeEventListener('focusin', this._handleAutoPauseFocusBound);
+        this.el.removeEventListener('focusout', this._handleAutoStartFocusBound);
+      }
+      if (this.options.pauseOnHover){
+        this.el.removeEventListener('mouseenter', this._handleAutoPauseHoverBound);
+        this.el.removeEventListener('mouseleave', this._handleAutoStartHoverBound);
+      }
       if (this.options.indicators) {
         this.$indicators.each((el) => {
-          el.removeEventListener('click', this._handleIndicatorClickBound);
+          el.children[0].removeEventListener('click', this._handleIndicatorClickBound);
         });
       }
     }
@@ -161,8 +209,49 @@
      * @param {Event} e
      */
     _handleIndicatorClick(e) {
-      let currIndex = $(e.target).index();
+      let currIndex = $(e.target).parent().index();
+      this._focusCurrent = true;
       this.set(currIndex);
+    }
+
+    /**
+     * Mouse enter event handler
+     */
+    _handleAutoPauseHover() {
+      this._hovered = true;
+      if (this.interval != null){
+        this._pause(true);
+      }
+    }
+
+    /**
+     * Focus in event handler
+     */
+    _handleAutoPauseFocus() {
+      this._focused = true;
+      if (this.interval != null){
+        this._pause(true);
+      }
+    }
+
+    /**
+     *  Mouse enter event handler
+     */
+    _handleAutoStartHover() {
+      this._hovered = false;
+      if (!(this.options.pauseOnFocus && this._focused) && this.eventPause){
+        this.start();
+      }
+    }
+
+    /**
+     *  Focus out leave event handler
+     */
+     _handleAutoStartFocus() {
+      this._focused = false;
+      if (!(this.options.pauseOnHover && this._hovered) && this.eventPause){
+        this.start();
+      }
     }
 
     /**
@@ -223,8 +312,11 @@
     _setupIndicators() {
       if (this.options.indicators) {
         this.$indicators = $('<ul class="indicators"></ul>');
-        this.$slides.each((el, index) => {
-          let $indicator = $('<li class="indicator-item"></li>');
+        this.$slides.each((el, i) => {
+          let label = this.options.indicatorLabelFunc ? this.options.indicatorLabelFunc.call(this, i + 1, i === 0) : `${i + 1}`;
+          let $indicator = $(`<li class="indicator-item">
+            <button type="button" class="indicator-item-btn" aria-label="${label}" aria-controls="${this._sliderId}"></button>
+          </li>`);
           this.$indicators.append($indicator[0]);
         });
         this.$el.append(this.$indicators[0]);
@@ -253,6 +345,10 @@
         this.$active = this.$slides.eq(this.activeIndex);
         let $caption = this.$active.find('.caption');
         this.$active.removeClass('active');
+        // Enables every slide
+        this.$slides.each((el) => {
+          el.style.visibility = 'visible';
+        });
 
         anim({
           targets: this.$active[0],
@@ -269,6 +365,8 @@
                 duration: 0,
                 easing: 'easeOutQuad'
               });
+              // Disables invisible slides (for assistive technologies)
+              el.style.visibility = 'hidden';
             });
           }
         });
@@ -277,8 +375,14 @@
 
         // Update indicators
         if (this.options.indicators) {
-          this.$indicators.eq(this.activeIndex).removeClass('active');
-          this.$indicators.eq(index).addClass('active');
+          let oActive = this.$indicators.eq(this.activeIndex).children().first()[0];
+          let nActive = this.$indicators.eq(index).children().first()[0];
+          oActive.classList.remove('active');
+          nActive.classList.add('active');
+          if (typeof this.options.indicatorLabelFunc === "function"){
+            oActive.setAttribute("aria-label", this.options.indicatorLabelFunc.call(this, this.$indicators.eq(this.activeIndex).index(), false));
+            nActive.setAttribute("aria-label", this.options.indicatorLabelFunc.call(this, this.$indicators.eq(index).index(), true));
+          }
         }
 
         anim({
@@ -299,18 +403,35 @@
         });
 
         this.$slides.eq(index).addClass('active');
+        if (this._focusCurrent){
+          this.$slides.eq(index)[0].focus();
+          this._focusCurrent = false;
+        }
         this.activeIndex = index;
 
-        // Reset interval
-        this.start();
+        // Reset interval, if allowed. This check prevents autostart
+        // when slider is paused, since it can be changed though indicators.
+        if (this.interval != null){
+          this.start();
+        }
       }
+    }
+
+    /**
+     * "Protected" function which pauses current interval
+     * @param {boolean} fromEvent Specifies if request came from event
+     */
+    _pause(fromEvent) {
+      clearInterval(this.interval);
+      this.eventPause = fromEvent;
+      this.interval = null;
     }
 
     /**
      * Pause slider interval
      */
     pause() {
-      clearInterval(this.interval);
+      this._pause(false);
     }
 
     /**
@@ -322,6 +443,7 @@
         this._handleIntervalBound,
         this.options.duration + this.options.interval
       );
+      this.eventPause = false;
     }
 
     /**
