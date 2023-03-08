16697c33b8042b194635b1cdb04b9af1ff034e4e
Daniel Wurzer <wurzer.da@gmail.com>
2023-03-08 21:00:05 +0100

    refactor: removed unused code

--- docs/js/materialize.js
+++ docs/js/materialize.js
@@ -7063,52 +7063,6 @@ $jscomp.polyfill = function (e, r, p, m) {
   }
 })(cash);
 ;(function ($) {
-  var TEXT_BASED_INPUT_SELECTOR = ['input:not([type])', 'input[type=text]', 'input[type=password]', 'input[type=email]', 'input[type=url]', 'input[type=tel]', 'input[type=number]', 'input[type=search]', 'input[type=date]', 'input[type=time]', 'input[type=month]', 'input[type=datetime-local]', 'textarea'].join(',');
-
-  // Function to update labels of text fields
-  M.updateTextFields = function () {
-    /*
-    $(TEXT_BASED_INPUT_SELECTOR).each(function(element, index) {
-      let $this = $(this);
-      if (
-        element.value.length > 0 ||
-        $(element).is(':focus') ||
-        element.autofocus ||
-        $this.attr('placeholder') !== null
-      ) {
-        $this.siblings('label').addClass('active');
-      } else if (element.validity) {
-        $this.siblings('label').toggleClass('active', element.validity.badInput === true);
-      } else {
-        $this.siblings('label').removeClass('active');
-      }
-    });
-    */
-  };
-
-  M.validate_field = function (object) {
-    var hasLength = object.attr('data-length') !== null;
-    var lenAttr = parseInt(object.attr('data-length'));
-    var len = object[0].value.length;
-
-    if (len === 0 && object[0].validity.badInput === false && !object.is(':required')) {
-      if (object.hasClass('validate')) {
-        object.removeClass('valid');
-        object.removeClass('invalid');
-      }
-    } else {
-      if (object.hasClass('validate')) {
-        // Check for character counter attributes
-        if (object.is(':valid') && hasLength && len <= lenAttr || object.is(':valid') && !hasLength) {
-          object.removeClass('invalid');
-          object.addClass('valid');
-        } else {
-          object.removeClass('valid');
-          object.addClass('invalid');
-        }
-      }
-    }
-  };
 
   M.textareaAutoResize = function ($textarea) {
     // Wrap if native element
@@ -7132,34 +7086,19 @@ $jscomp.polyfill = function (e, r, p, m) {
     var fontFamily = $textarea.css('font-family');
     var fontSize = $textarea.css('font-size');
     var lineHeight = $textarea.css('line-height');
-
     // Firefox can't handle padding shorthand.
     var paddingTop = $textarea.css('padding-top');
     var paddingRight = $textarea.css('padding-right');
     var paddingBottom = $textarea.css('padding-bottom');
     var paddingLeft = $textarea.css('padding-left');
 
-    if (fontSize) {
-      hiddenDiv.css('font-size', fontSize);
-    }
-    if (fontFamily) {
-      hiddenDiv.css('font-family', fontFamily);
-    }
-    if (lineHeight) {
-      hiddenDiv.css('line-height', lineHeight);
-    }
-    if (paddingTop) {
-      hiddenDiv.css('padding-top', paddingTop);
-    }
-    if (paddingRight) {
-      hiddenDiv.css('padding-right', paddingRight);
-    }
-    if (paddingBottom) {
-      hiddenDiv.css('padding-bottom', paddingBottom);
-    }
-    if (paddingLeft) {
-      hiddenDiv.css('padding-left', paddingLeft);
-    }
+    if (fontSize) hiddenDiv.css('font-size', fontSize);
+    if (fontFamily) hiddenDiv.css('font-family', fontFamily);
+    if (lineHeight) hiddenDiv.css('line-height', lineHeight);
+    if (paddingTop) hiddenDiv.css('padding-top', paddingTop);
+    if (paddingRight) hiddenDiv.css('padding-right', paddingRight);
+    if (paddingBottom) hiddenDiv.css('padding-bottom', paddingBottom);
+    if (paddingLeft) hiddenDiv.css('padding-left', paddingLeft);
 
     // Set original-height, if none
     if (!$textarea.data('original-height')) {
@@ -7201,93 +7140,6 @@ $jscomp.polyfill = function (e, r, p, m) {
   };
 
   $(document).ready(function () {
-    // Add active if form auto complete
-    $(document).on('change', TEXT_BASED_INPUT_SELECTOR, function () {
-      /*
-      if (this.value.length !== 0 || $(this).attr('placeholder') !== null) {
-        $(this)
-          .siblings('label')
-          .addClass('active');
-      }
-      */
-      M.validate_field($(this));
-    });
-
-    // Add active if input element has been pre-populated on document ready
-    $(document).ready(function () {
-      M.updateTextFields();
-    });
-
-    // HTML DOM FORM RESET handling
-    $(document).on('reset', function (e) {
-      var formReset = $(e.target);
-      if (formReset.is('form')) {
-        formReset.find(TEXT_BASED_INPUT_SELECTOR).removeClass('valid').removeClass('invalid');
-        formReset.find(TEXT_BASED_INPUT_SELECTOR).each(function (e) {
-          /*
-          if (this.value.length) {
-            $(this)
-              .siblings('label')
-              .removeClass('active');
-          }
-          */
-        });
-
-        // Reset select (after native reset)
-        setTimeout(function () {
-          formReset.find('select').each(function () {
-            // check if initialized
-            if (this.M_FormSelect) {
-              $(this).trigger('change');
-            }
-          });
-        }, 0);
-      }
-    });
-
-    /**
-     * Add active when element has focus
-     * @param {Event} e
-     */
-    /*
-    document.addEventListener(
-      'focus',
-      function(e) {
-        if ($(e.target).is(TEXT_BASED_INPUT_SELECTOR)) {
-          $(e.target)
-            .siblings('label, .prefix')
-            .addClass('active');
-        }
-      },
-      true
-    );
-    */
-
-    /**
-     * Remove active when element is blurred
-     * @param {Event} e
-     */
-    /*
-    document.addEventListener(
-      'blur',
-      function(e) {
-        let $inputElement = $(e.target);
-        if ($inputElement.is(TEXT_BASED_INPUT_SELECTOR)) {
-          let selector = '.prefix';
-           if (
-            $inputElement[0].value.length === 0 &&
-            $inputElement[0].validity.badInput !== true &&
-            $inputElement.attr('placeholder') === null
-          ) {
-            selector += ', label';
-          }
-          $inputElement.siblings(selector).removeClass('active');
-          M.validate_field($inputElement);
-        }
-      },
-      true
-    );
-    */
 
     // Radio and Checkbox focus class
     var radio_checkbox = 'input[type=radio], input[type=checkbox]';
