1235e3224198bb394226a947e61e1cc0fcdf8148
Daniel Wurzer <wurzer.da@gmail.com>
2023-03-17 09:17:04 +0100

    refactor(collapsible): removed jquery

--- src/collapsible.ts
+++ src/collapsible.ts
@@ -1,8 +1,7 @@
 import { Component } from "./component";
-import $ from "cash-dom";
 import anim from "animejs";
 
-let _defaults = {
+const _defaults = {
   accordion: true,
   onOpenStart: undefined,
   onOpenEnd: undefined,
@@ -13,7 +12,7 @@ let _defaults = {
 };
 
 export class Collapsible extends Component {
-  $headers: any;
+  private _headers: HTMLElement[];
   private _handleCollapsibleClickBound: any;
   private _handleCollapsibleKeydownBound: any;
 
@@ -21,22 +20,17 @@ export class Collapsible extends Component {
     super(Collapsible, el, options);
     (this.el as any).M_Collapsible = this;
     this.options = {...Collapsible.defaults, ...options};
-
     // Setup tab indices
-    this.$headers = this.$el.children('li').children('.collapsible-header');
-    this.$headers.attr('tabindex', 0);
-
+    this._headers = Array.from(this.el.querySelectorAll('li > .collapsible-header'));
+    this._headers.forEach(el => el.tabIndex = 0);
     this._setupEventHandlers();
-
     // Open first active
-    let $activeBodies = this.$el.children('li.active').children('.collapsible-body');
-    if (this.options.accordion) {
-      // Handle Accordion
-      $activeBodies.first().css('display', 'block');
-    } else {
-      // Handle Expandables
-      $activeBodies.css('display', 'block');
-    }
+    const activeBodies: HTMLElement[] = Array.from(this.el.querySelectorAll('li.active > .collapsible-body'));
+    if (this.options.accordion)
+      if (activeBodies.length > 0)
+        activeBodies[0].style.display = 'block'; // Accordion
+    else 
+      activeBodies.forEach(el => el.style.display = 'block'); // Expandables
   }
 
   static get defaults() {
@@ -48,7 +42,7 @@ export class Collapsible extends Component {
   }
 
   static getInstance(el) {
-    let domElem = !!el.jquery ? el[0] : el;
+    const domElem = !!el.jquery ? el[0] : el;
     return domElem.M_Collapsible;
   }
 
@@ -61,34 +55,28 @@ export class Collapsible extends Component {
     this._handleCollapsibleClickBound = this._handleCollapsibleClick.bind(this);
     this._handleCollapsibleKeydownBound = this._handleCollapsibleKeydown.bind(this);
     this.el.addEventListener('click', this._handleCollapsibleClickBound);
-    this.$headers.each((i, header) => {
-      header.addEventListener('keydown', this._handleCollapsibleKeydownBound);
-    });
+    this._headers.forEach(header => header.addEventListener('keydown', this._handleCollapsibleKeydownBound));
   }
 
   _removeEventHandlers() {
     this.el.removeEventListener('click', this._handleCollapsibleClickBound);
-    this.$headers.each((i, header) => {
-      header.removeEventListener('keydown', this._handleCollapsibleKeydownBound);
-    });
+    this._headers.forEach(header => header.removeEventListener('keydown', this._handleCollapsibleKeydownBound));
   }
 
   _handleCollapsibleClick(e) {
-    let $header = $(e.target).closest('.collapsible-header');
-    if (e.target && $header.length) {
-      let $collapsible = $header.closest('.collapsible');
-      if ($collapsible[0] === this.el) {
-        let $collapsibleLi = $header.closest('li');
-        let $collapsibleLis = $collapsible.children('li');
-        let isActive = $collapsibleLi[0].classList.contains('active');
-        let index = $collapsibleLis.index($collapsibleLi);
+    const header = e.target.closest('.collapsible-header');
+    if (e.target && header) {
+      const collapsible = header.closest('.collapsible');
+      if (collapsible !== this.el) return;
 
-        if (isActive) {
-          this.close(index);
-        } else {
-          this.open(index);
-        }
-      }
+      const li = header.closest('li');
+      const isActive = li.classList.contains('active');
+      const index = [...li.parentNode.children].indexOf(li);
+
+      if (isActive)
+        this.close(index);
+      else
+        this.open(index);
     }
   }
 
@@ -99,110 +87,97 @@ export class Collapsible extends Component {
   }
 
   _animateIn(index: number) {
-    let $collapsibleLi = this.$el.children('li').eq(index);
-    if ($collapsibleLi.length) {
-      let $body = $collapsibleLi.children('.collapsible-body');
-
-      anim.remove($body[0]);
-      $body.css({
-        display: 'block',
-        overflow: 'hidden',
-        height: 0,
-        paddingTop: '',
-        paddingBottom: ''
-      });
-
-      let pTop = $body.css('padding-top');
-      let pBottom = $body.css('padding-bottom');
-      let finalHeight = $body[0].scrollHeight;
-      $body.css({
-        paddingTop: 0,
-        paddingBottom: 0
-      });
-      anim({
-        targets: $body[0],
-        height: finalHeight,
-        paddingTop: pTop,
-        paddingBottom: pBottom,
-        duration: this.options.inDuration,
-        easing: 'easeInOutCubic',
-        complete: (anim) => {
-          $body.css({
-            overflow: '',
-            paddingTop: '',
-            paddingBottom: '',
-            height: ''
-          });
-          // onOpenEnd callback
-          if (typeof this.options.onOpenEnd === 'function') {
-            this.options.onOpenEnd.call(this, $collapsibleLi[0]);
-          }
+    const li = this.el.children[index];
+    if (!li) return;
+    const body: HTMLElement = li.querySelector('.collapsible-body');
+    anim.remove(body);
+    body.style.display = 'block';
+    body.style.overflow = 'hidden';
+    body.style.height = '0';
+    body.style.paddingTop = '';
+    body.style.paddingBottom = '';
+    const pTop = getComputedStyle(body).paddingTop; //  . css('padding-top');
+    const pBottom = getComputedStyle(body).paddingBottom; //body.css('padding-bottom');
+    const finalHeight = body.scrollHeight;
+    body.style.paddingTop = '0';
+    body.style.paddingBottom = '0';
+    anim({
+      targets: body,
+      height: finalHeight,
+      paddingTop: pTop,
+      paddingBottom: pBottom,
+      duration: this.options.inDuration,
+      easing: 'easeInOutCubic',
+      complete: (anim) => {
+        body.style.overflow = '';
+        body.style.height = '';
+        body.style.paddingTop = '';
+        body.style.paddingBottom = '';
+        // onOpenEnd callback
+        if (typeof this.options.onOpenEnd === 'function') {
+          this.options.onOpenEnd.call(this, li);
         }
-      });
-    }
+      }
+    });
   }
 
   _animateOut(index: number) {
-    let $collapsibleLi = this.$el.children('li').eq(index);
-    if ($collapsibleLi.length) {
-      let $body = $collapsibleLi.children('.collapsible-body');
-      anim.remove($body[0]);
-      $body.css('overflow', 'hidden');
-      anim({
-        targets: $body[0],
-        height: 0,
-        paddingTop: 0,
-        paddingBottom: 0,
-        duration: this.options.outDuration,
-        easing: 'easeInOutCubic',
-        complete: () => {
-          $body.css({
-            height: '',
-            overflow: '',
-            padding: '',
-            display: ''
-          });
-          // onCloseEnd callback
-          if (typeof this.options.onCloseEnd === 'function') {
-            this.options.onCloseEnd.call(this, $collapsibleLi[0]);
-          }
+    const li = this.el.children[index];
+    if (!li) return;
+    const body: HTMLElement = li.querySelector('.collapsible-body');
+    anim.remove(body);
+    body.style.overflow = 'hidden';
+    anim({
+      targets: body,
+      height: 0,
+      paddingTop: 0,
+      paddingBottom: 0,
+      duration: this.options.outDuration,
+      easing: 'easeInOutCubic',
+      complete: () => {
+        body.style.overflow = '';
+        body.style.height = '';
+        body.style.padding = '';
+        body.style.display = '';
+        // onCloseEnd callback
+        if (typeof this.options.onCloseEnd === 'function') {
+          this.options.onCloseEnd.call(this, li);
         }
-      });
-    }
+      }
+    });
   }
 
   open(index: number) {
-    let $collapsibleLi = this.$el.children('li').eq(index);
-    if ($collapsibleLi.length && !$collapsibleLi[0].classList.contains('active')) {
+    const listItems = Array.from(this.el.children).filter(c => c.tagName === 'LI');
+    const li = listItems[index];
+    if (li && !li.classList.contains('active')) {
       // onOpenStart callback
       if (typeof this.options.onOpenStart === 'function') {
-        this.options.onOpenStart.call(this, $collapsibleLi[0]);
+        this.options.onOpenStart.call(this, li);
       }
       // Handle accordion behavior
       if (this.options.accordion) {
-        let $collapsibleLis = this.$el.children('li');
-        let $activeLis = this.$el.children('li.active');
-        $activeLis.each((i, el) => {
-          let index = $collapsibleLis.index($(el));
+        const activeLis = listItems.filter(li => li.classList.contains('active'));
+        activeLis.forEach(activeLi => {
+          const index = listItems.indexOf(activeLi);
           this.close(index);
         });
       }
       // Animate in
-      $collapsibleLi[0].classList.add('active');
+      li.classList.add('active');
       this._animateIn(index);
     }
   }
 
   close(index: number) {
-    let $collapsibleLi = this.$el.children('li').eq(index);
-    if ($collapsibleLi.length && $collapsibleLi[0].classList.contains('active')) {
+    const li = Array.from(this.el.children).filter(c => c.tagName === 'LI')[index];
+    if (li && li.classList.contains('active')) {
       // onCloseStart callback
       if (typeof this.options.onCloseStart === 'function') {
-        this.options.onCloseStart.call(this, $collapsibleLi[0]);
+        this.options.onCloseStart.call(this, li);
       }
-
       // Animate out
-      $collapsibleLi[0].classList.remove('active');
+      li.classList.remove('active');
       this._animateOut(index);
     }
   }
