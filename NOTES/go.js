6eb284ce37423373b2ee1b85cdd1f3cd62e6e024
Michael J. Frankham <michael.frankham@praemium.com>
2023-07-26 22:02:33 +1000

    STUFF

---
+++ NOTES/go.js
@@ -0,0 +1,62 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-console */
+
+const values = [
+    ['Color', 'COLOR', 'color_content.html'],
+    ['Grid', 'GRID', 'grid_content.html'],
+    ['Helpers', 'HELPERS', 'helpers_content.html'],
+    ['Media CSS', 'MEDIA_CSS', 'media-css_content.html'],
+    ['Pulse', 'PULSE', 'pulse_content.html'],
+    ['Sass', 'SASS', 'sass_content.html'],
+    ['Shadow', 'SHADOW', 'shadow_content.html'],
+    ['Table', 'TABLE', 'table_content.html'],
+    [
+        'CSS Transitions',
+        'CSS_TRANSITIONS',
+        'css-transitions_content.html'
+    ],
+    ['Typography', 'TYPOGRAPHY', 'typography_content.html'],
+    ['Waves', 'WAVES', 'waves_content.html'],
+    ['Badges', 'BADGES', 'badges_content.html'],
+    ['Buttons', 'BUTTONS', 'buttons_content.html'],
+    ['Breadcrumbs', 'BREADCRUMBS', 'breadcrumbs_content.html'],
+    ['Cards', 'CARDS', 'cards_content.html'],
+    ['Collections', 'COLLECTIONS', 'collections_content.html'],
+    [
+        'Floating Action Button',
+        'FLOATING_ACTION_BUTTON',
+        'floating-action-button-content.html'
+    ],
+    ['Footer', 'FOOTER', 'footer_content.html'],
+    ['Icons', 'ICONS', 'icons_content.html'],
+    ['Navbar', 'NAVBAR', 'navbar_content.html'],
+    ['Pagination', 'PAGINATION', 'pagination_content.html'],
+    ['Preloader', 'PRELOADER', 'preloader_content.html'],
+    ['Auto Init', 'AUTO_INIT', 'auto_init_content.html'],
+    ['Carousel', 'CAROUSEL', 'carousel_content.html'],
+    ['Collapsible', 'COLLAPSIBLE', 'collapsible_content.html'],
+    ['Dropdown', 'DROPDOWN', 'dropdown_content.html'],
+    [
+        'Feature Discovery',
+        'FEATURE_DISCOVERY',
+        'featureDiscovery_content.html'
+    ],
+    ['Media', 'MEDIA', 'media_content.html'],
+    ['Modals', 'MODALS', 'modals_content.html'],
+    ['Parallax', 'PARALLAX', 'parallax_content.html'],
+    ['Pushpin', 'PUSHPIN', 'pushpin_content.html'],
+    ['Scrollspy', 'SCROLLSPY', 'scrollspy_content.html'],
+    ['Sidenav', 'SIDENAV', 'sidenav_content.html'],
+    ['Tabs', 'TABS', 'tabs_content.html'],
+    ['Toasts', 'TOASTS', 'toasts_content.html'],
+    ['Tooltips', 'TOOLTIPS', 'tooltips_content.html'],
+    ['Autocomplete', 'AUTOCOMPLETE', 'autocomplete_content.html'],
+    ['Checkboxes', 'CHECKBOXES', 'checkboxes_content.html'],
+    ['Chips', 'CHIPS', 'chips_content.html'],
+    ['Pickers', 'PICKERS', 'pickers_content.html'],
+    ['Radio Buttons', 'RADIO_BUTTONS', 'radio_buttons_content.html'],
+    ['Range', 'RANGE', 'range_content.html'],
+    ['Select', 'SELECT', 'select_content.html'],
+    ['Switches', 'SWITCHES', 'switches_content.html'],
+    ['Text Inputs', 'TEXT_INPUTS', 'text_inputs_content.html']
+];
