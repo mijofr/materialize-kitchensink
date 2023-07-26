26d8ed393959c7c7287e7fbe79ded9afa797eeac
Michael J. Frankham <michael.frankham@praemium.com>
2023-07-26 18:50:22 +1000

    chore: go away

--- commitlint.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  helpUrl: "Confused? Try 'npm run-script commit'. For more info: https://github.com/conventional-changelog/commitlint/#what-is-commitlint"
-}
